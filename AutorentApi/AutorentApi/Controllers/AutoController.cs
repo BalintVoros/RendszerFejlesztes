f4eb11357a5582bea2e36f4749e4c8e74c7fe911
Vörös Bálint <91821323+BalintVoros@users.noreply.github.com>
2024-04-02 18:48:49 +0200

    Base

---
+++ AutorentApi/AutorentApi/Controllers/AutoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutorentApi.Models;
+
+namespace AutorentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoController : ControllerBase
+    {
+        private static readonly List<Auto> products = new List<Auto>
+        {
+            new Auto { Id = 1, Name = "Product 1", Price = 100 },
+            // Ide jöhetnek további termékek...
+        };
+
+        [HttpGet]
+        public ActionResult<List<Auto>> GetAll()
+        {
+            return products;
+        }
+
+        // Itt adhatsz hozzá további API végpontokat, például a termék hozzáadásához, frissítéséhez vagy törléséhez.
+    }
+}
---
+++ AutorentApi/AutorentApi/Models/Auto.cs
@@ -0,0 +1,9 @@
+﻿namespace AutorentApi.Models
+{
+    public class Auto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
