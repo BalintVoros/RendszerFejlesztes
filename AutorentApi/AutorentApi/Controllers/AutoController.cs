af88c0cf730c4d05dad71efbb1c8392753850587
Vörös Bálint <91821323+BalintVoros@users.noreply.github.com>
2024-04-03 11:37:41 +0200

    Correction

--- AutorentApi/AutorentApi/Controllers/AutoController.cs
+++ AutorentApi/AutorentApi/Controllers/AutoController.cs
@@ -10,7 +10,9 @@ namespace AutorentApi.Controllers
     {
         private static readonly List<Auto> products = new List<Auto>
         {
-            new Auto { Id = 1, Name = "Product 1", Price = 100 },
+            new Auto { Id = 1, Name = "Audi a3", Price = 100, Brand = "Audi", Model = "a3", DailyPrice = 30},
+            new Auto { Id = 2, Name = "BMW e320", Price = 200 , Brand = "BMW", Model = "e320", DailyPrice = 50},
+            new Auto { Id = 3, Name = "Mercedes c320", Price = 300 , Brand = "Mercedes", Model = "c320", DailyPrice = 100}
             // Ide jöhetnek további termékek...
         };
 
